f3140a514b78c1119b937a3a59366414e5d03b5c
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2025-03-31 20:44:23 -0300

    [feat] FISH plugin manager and Sponge plugin

--- fedora.sh
+++ fedora.sh
@@ -163,6 +163,10 @@ xfconf-query -c xfce4-session -p /sessions/Failsafe/Client3_Command -t string -s
 # Change shell to FISH
 chsh -s /bin/fish
 
+# Install FISH plugin manager and a plugin for cleaning history failed commands
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install meaningful-ooo/sponge
+
 # Remove unnecessary files from $HOME
 ls -a | grep bash | xargs -d "\n" rm -v
 
