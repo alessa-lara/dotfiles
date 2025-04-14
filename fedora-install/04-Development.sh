e5733d51ad4b60d6d2bfc2dd831a5dc03d4ab40c
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2025-04-14 19:43:30 -0300

    [feat] using clangd as my lsp

--- fedora-install/04-Development.sh
+++ fedora-install/04-Development.sh
@@ -1,5 +1,6 @@
 # Install C development packages
 sudo dnf group install -y --with-optional c-development
+sudo dnf install -y clang clang-tools-extra
 
 # Install Rust and Cross (Tool to easily cross-compile rust code via docker)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -13,6 +14,7 @@ dnf check-update
 sudo dnf install -y code
 code --extensions-dir=.config/Code/Extensions --install-extension fabiospampinato.vscode-monokai-night
 code --extensions-dir=.config/Code/Extensions --install-extension ms-vscode.cpptools
+code --extensions-dir=.config/Code/Extensions --install-extension llvm-vs-code-extensions.vscode-clangd
 code --extensions-dir=.config/Code/Extensions --install-extension rust-lang.rust-analyzer
 code --extensions-dir=.config/Code/Extensions --install-extension vadimcn.vscode-lldb
 code --extensions-dir=.config/Code/Extensions --install-extension aaron-bond.better-comments
