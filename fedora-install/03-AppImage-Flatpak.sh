af9d239906df53bac69f13166e8fbdabf2bf03bc
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2025-09-23 23:26:37 -0300

    [feat] obsidian install

--- fedora-install/03-AppImage-Flatpak.sh
+++ fedora-install/03-AppImage-Flatpak.sh
@@ -6,5 +6,6 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 flatpak install -y com.bitwarden.desktop \
     com.google.Chrome \
     org.mozilla.firefox \
-    dev.vencord.Vesktop
+    dev.vencord.Vesktop \
+    md.obsidian.Obsidian
 sudo flatpak override --filesystem=xdg-data/themes
