01c7712218b73dc3f44b54dd76bceb59998808c1
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-04-12 18:06:49 -0300

    [fix](disable, lspconfig) files where in the wrong dir

---
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "folke/persistence.nvim",
+        enabled = false,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        enabled = false,
+    },
+
+    {
+        "catppuccin/nvim",
+        enabled = false,
+    },
+
+    -- disabling keymaps
+    { "LazyVim/LazyVim", opts = { defaults = { keymaps = false } } },
+    { "MagicDuck/grug-far.nvim", keys = false },
+    { "mason-org/mason.nvim", keys = false },
+    { "nvim-mini/mini.surround", keys = false },
+    { "mfussenegger/nvim-dap", keys = false },
+    { "mfussenegger/nvim-dap-python", keys = false },
+    { "rcarriga/nvim-dap-ui", keys = false },
+    { "folke/todo-comments.nvim", keys = false },
+    { "linux-cultist/venv-selector.nvim", keys = false },
+    { "iamcco/markdown-preview.nvim", keys = false },
+    { "stevearc/conform.nvim", keys = false },
+    { "folke/which-key.nvim", keys = false },
+    { "folke/trouble.nvim", keys = false },
+    { "folke/flash.nvim", keys = false },
+    { "folke/noice.nvim", keys = false },
+    { "nvim-lualine/lualine.nvim", keys = false },
+    { "nvim-treesitter/nvim-treesitter-textobjects", keys = false },
+    { "stevearc/overseer.nvim", keys = false },
+    { "folke/snacks.nvim", keys = false },
+}
