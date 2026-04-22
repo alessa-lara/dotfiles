2df5e6b7cb9ec97417dd152ba098dd5e2af1ac6c
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-04-21 22:08:32 -0300

    [feat] colors for markdown italic and bold

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,20 @@
 return {
-    {
-        "sainnhe/sonokai",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.sonokai_style = "maia"
-            vim.g.sonokai_transparent_background = "1"
-            vim.cmd.colorscheme("sonokai")
-        end,
-    },
+	{
+		"sainnhe/sonokai",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_style = "maia"
+			vim.g.sonokai_transparent_background = "1"
+			vim.g.sonokai_enable_italic = 1
+			vim.g.sonokai_enable_bold = 1
+			vim.cmd.colorscheme("sonokai")
+
+			-- define highlight for markdown bold and italic
+			local blue = vim.api.nvim_get_hl(0, { name = "Blue" }).fg
+			local red = vim.api.nvim_get_hl(0, { name = "Red" }).fg
+			vim.api.nvim_set_hl(0, "TSStrong", { fg = red, bold = true })
+			vim.api.nvim_set_hl(0, "@markup.italic", { fg = blue, italic = true })
+		end,
+	},
 }
