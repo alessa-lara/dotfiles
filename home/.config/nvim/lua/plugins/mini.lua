63a1feaaa744c1e091c23a193e6f5a55e138d3da
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-06-09 14:58:01 -0300

    [feat](mini hippatterns) config for the plugin

--- home/.config/nvim/lua/plugins/mini.lua
+++ home/.config/nvim/lua/plugins/mini.lua
@@ -60,7 +60,18 @@ return {
 
 	{ "nvim-mini/mini.ai", opts = {} },
 
-	{ "nvim-mini/mini.hipatterns", lazy = false, opts = {} },
+	{
+		"nvim-mini/mini.hipatterns",
+		lazy = false,
+		opts = {
+			highlighters = {
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+			},
+		},
+	},
 
 	{ "nvim-mini/mini.icons", lazy = false, opts = {} },
 
